57ed3fb0adcc45f2a002ede14aacca52299a614c
Aaron Traas <aaron.traas@ogilvy.com>
2018-06-11 12:16:08 -0400

    Some cleanups

--- src/popup.js
+++ src/popup.js
@@ -2,32 +2,48 @@
 
 let buttonRandomActions = document.getElementById('randomizeActions');
 
-buttonRandomActions.onclick = function(element) {
-	let color = element.target.value;
-	let script = "\
-		var verbs = ['EATS', 'DRINKS', 'JUGGLES', 'ROASTS', 'FROLICKS through'];\
-		var objects = ['mummified cats', 'pretzels', 'herring', 'the fjords'];\
-		var elements = document.querySelectorAll('#orders_list_overlay_div_text>span');\
-		var link = '<a href=\"#\">(X)</a>';\
-		for (var i = 0; i < elements.length; i += 1) {\
-			content = elements[i].innerHTML;\
-			var index = -1;\
-			index = content.indexOf('MOVE');\
-			if(index===-1) {index = content.indexOf('SUPPORT')}\
-			if(index===-1) {index = content.indexOf('HOLD')}\
-			if(index > 1) {\
-				verb = 'VERB';\
-				var verb = verbs[Math.floor(Math.random()*verbs.length)];\
-				object = objects[Math.floor(Math.random()*objects.length)];\;\
-    			content = content.substring(0,index) + verb + ' ' + object + ' ' + link;\
+let verbs = ['EATS', 'DRINKS', 'JUGGLES', 'ROASTS', 'FROLICKS through'];
+let objects = ['mummified cats', 'pretzels', 'herring', 'the fjords'];
+var commands = ['MOVE', 'SUPPORT', 'CONVOY', 'HOLD'];
+
+let verbs_string =  "['" + verbs.join("','") + "']";
+let objects_string = "['" + objects.join("','") + "']";
+let commands_string = "['" + commands.join("','") + "']";
+
+let script_random_actions = "\
+	var commands = " + commands_string + ";\
+	var verbs = " + verbs_string + ";\
+	var objects = " + objects_string + ";\
+	var link = '<a href=\"#\">(X)</a>';\
+	\
+	var elements = document.querySelectorAll('#orders_list_overlay_div_text>span');\
+	elements.forEach( function(element) {\
+		content = element.innerHTML;\
+		var index = -1;\
+		commands.forEach( function(command) {\
+			if(index===-1) {\
+				index = content.indexOf(command);\
 			}\
-    		console.log( content );\
-    		elements[i].innerHTML = content;\
-    	}\
-	";
+		});\
+		\
+		if(index > 1) {\
+			verb = 'VERB';\
+			var verb = verbs[Math.floor(Math.random()*verbs.length)];\
+			object = objects[Math.floor(Math.random()*objects.length)];\
+			content = content.substring(0,index) + verb + ' ' + object + ' ' + link;\
+			console.log( content );\
+			element.innerHTML = content;\
+		}\
+	});\
+";
+
+
+buttonRandomActions.onclick = function(element) {
+
+
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.executeScript(
 			tabs[0].id,
-			{code: script});
+			{code: script_random_actions});
 	});
 };
